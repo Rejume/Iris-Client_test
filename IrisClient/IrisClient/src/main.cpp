8fcd92916d981bcd677b94e365e7bf45d3eecbc5
nejime <halnejime@gmail.com>
2016-02-24 13:11:06 +0900

    no message

--- IrisClient/IrisClient/src/main.cpp
+++ IrisClient/IrisClient/src/main.cpp
@@ -1,6 +1,10 @@
+//#include <boost_1_60_0/boost/regex.h>
 
-
+#include <boost/regex.h>
+#include <boost/version.hpp>
+//#include <boost>
 int main() {
-	
+	std::cout << "hoge";
+	std::cout << (BOOST_VERSION);
 	return 0;
 }
